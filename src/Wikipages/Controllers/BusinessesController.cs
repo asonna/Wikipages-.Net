2ffa193bbcea0a34e5d6cdb0ac0df368e8130f1b
Annie Sonna and Chris Wingo <annie+chris@>
2017-02-06 17:32:42 -0800

    Continue setting up files and add the script.sql file as well

---
+++ src/Wikipages/Controllers/BusinessesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Wikipages.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Wikipages.Controllers
+{
+    public class BusinessesController : Controller
+    {
+        private WikipagesContext db = new WikipagesContext();
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(db.Businesses.ToList());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var thisBusiness = db.Businesses.FirstOrDefault(businesses => businesses.BusinessId == id);
+            return View(thisBusiness);
+        }
+    }
+}
---
+++ src/Wikipages/Models/Business.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wikipages.Models
+{
+    [Table("Businesses")]
+    public class Business
+    {
+        [Key]
+        public int BusinessId { get; set; }
+        public string Type { get; set; }
+    }
+}
